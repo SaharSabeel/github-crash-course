6fe4a5b0325eda1348180d5178826ca994995e37
sahar sabeel <softosahar@gmail.com>
2024-05-04 12:10:13 +0500

    new function added

---
+++ index.js
@@ -0,0 +1,11 @@
+const name1 = " hello world";
+const newName = 'sabeel'
+function sayName (){
+    console.log('my name is ', name1)
+}
+sayName()
+console.log(newName)
+function me(){
+    console.log('this is my js')
+}
+me()
