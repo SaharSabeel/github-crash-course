7068d7ac4d1130a678f9c5c88537a11bf225b440
sahar sabeel <sapienktk@gmail.com>
2024-05-05 11:03:16 +0500

    pull method git

--- new.js
+++ new.js
@@ -6,3 +6,13 @@ function newjs(name){
     console.log(name)
 }
 newjs('shajjr')
+const array1 =[57699,69770707];
+function newjs(name){
+    console.log(name)
+}
+newjs('shajjr')
+function newjs1(name){
+    console.log(name)
+}
+newjs1('shajjr')
+const array =[57699,69770707];
